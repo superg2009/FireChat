c934cd81e314ef12a076304461cbddcae9b1cfc9
Grant Marshall <grantrmarshall97@gmail.com>
2017-11-07 23:05:11 -0800

    a proper start

---
+++ app/src/main/java/trufire/com/firechat/HubActivity.java
@@ -0,0 +1,14 @@
+package trufire.com.firechat;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class HubActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hub);
+    }
+
+}
--- app/src/main/java/trufire/com/firechat/MainActivity.java
+++ app/src/main/java/trufire/com/firechat/MainActivity.java
@@ -1,38 +1,66 @@
 package trufire.com.firechat;
 
-import android.nfc.Tag;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-
 import com.firebase.ui.*;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.*;
-import com.google.firebase.auth.AuthResult;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity{
     private  String TAG= "firechat.MainActivity";
+    private final static int RC_SIGN_IN=123;
     private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mAuth=FirebaseAuth.getInstance();
+         mAuth=FirebaseAuth.getInstance();
+         if(mAuth.getCurrentUser()!=null){
+                startActivity(new Intent(MainActivity.this,HubActivity.class));
+         }else {
+             //not signed in
+             startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().setIsSmartLockEnabled(!BuildConfig.DEBUG).build(),RC_SIGN_IN);
+         }
     }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        FirebaseUser currentUser= mAuth.getCurrentUser();
-        if(currentUser==null){
-            startLogin();
+            // Successfully signed in
+            if (resultCode == RESULT_OK) {
+                //startActivity();
+                finish();
+                return;
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    Toast.makeText(getApplicationContext(),"Sign in failed",Toast.LENGTH_SHORT);
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    Toast.makeText(getApplicationContext(),"No Internet Connection",Toast.LENGTH_SHORT);
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    Toast.makeText(getApplicationContext(),"Unknown error occurred",Toast.LENGTH_SHORT);
+                    return;
+                }
+            }
+
+
         }
     }
-    protected void startLogin(){
 
-    }
 }
